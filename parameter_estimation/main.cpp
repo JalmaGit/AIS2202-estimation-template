78b7a8376250367de10fb187bf7bfc9f83f3c58c
Huqo7 <vegfor07@gmail.com>
2024-10-07 10:17:26 +0200

    added data files

--- parameter_estimation/main.cpp
+++ parameter_estimation/main.cpp
@@ -1,13 +1,16 @@
-#include <rapidcsv.h>
 #include <Eigen/Dense>
+#include <rapidcsv.h>
 
-Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A)
-{
-    return A.completeOrthogonalDecomposition().pseudoInverse();
+Eigen::MatrixXd pseudo_inverse(const Eigen::MatrixXd &A) {
+  return A.completeOrthogonalDecomposition().pseudoInverse();
 }
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main() {
+  rapidcsv::Document accel("data/1-baseline_accel.csv");
+  rapidcsv::Document wrench("data/1-baseline_wrench.csv");
+  rapidcsv::Document orientations("data/1-baseline_orientations.csv");
+
+  Eigen::Vector3f gravity(0, 0 - 9.81);
+
+  return 0;
 }
