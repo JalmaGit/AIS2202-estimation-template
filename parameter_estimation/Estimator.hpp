6904e85d25c85f763e731e737fd2eef63da998dd
TeemiMe <siljem03@hotmail.com>
2024-10-10 00:37:50 +0200

    sensor bias
    estimerte sensor bias og printet ut

--- parameter_estimation/Estimator.hpp
+++ parameter_estimation/Estimator.hpp
@@ -5,22 +5,29 @@
 #include <rapidcsv.h>
 class Estimator {
 public:
-  Estimator(rapidcsv::Document &data);
-  float get_m();
-  Eigen::VectorXd get_r();
+    Estimator(rapidcsv::Document &data);
+    float get_m();
+    Eigen::VectorXd get_r();
+    Eigen::Vector3d get_force_bias();
+    Eigen::Vector3d get_torque_bias();
+    Eigen::Vector3d get_accel_bias();
 
 private:
-  void populate(rapidcsv::Document &data);
-  void calculate_m();
-  void calculate_r();
+    void populate(rapidcsv::Document &data);
+    void calculate_m();
+    void calculate_r();
+    void calculate_biases();
 
 private:
-  Eigen::VectorXd m_force_all;
-  Eigen::VectorXd m_torque_all;
-  Eigen::VectorXd m_gravity_all;
-  Eigen::MatrixXd m_A_all;
-  Eigen::VectorXd m_r;
-  float m_m;
+    Eigen::VectorXd m_force_all;
+    Eigen::VectorXd m_torque_all;
+    Eigen::VectorXd m_gravity_all;
+    Eigen::MatrixXd m_A_all;
+    Eigen::Vector3d m_force_bias;
+    Eigen::Vector3d m_torque_bias;
+    Eigen::Vector3d m_accel_bias;
+    Eigen::VectorXd m_r;
+    float m_m;
 };
 
 #endif
--- parameter_estimation/main.cpp
+++ parameter_estimation/main.cpp
@@ -1,12 +1,17 @@
-#include "Estimator.hpp"
-#include <Eigen/Dense>
 #include <rapidcsv.h>
+#include <Eigen/Dense>
+#include <iostream>
+#include "Estimator.hpp"
 
 int main() {
-  rapidcsv::Document data("data/0-calibration_fts-accel.csv");
+    rapidcsv::Document data("data/0-calibration_fts-accel.csv");
 
-  Estimator estimator(data);
-  std::cout << estimator.get_m() << std::endl;
-  std::cout << estimator.get_r() << std::endl;
-  return 0;
+    Estimator estimator(data);
+    std::cout << estimator.get_m() << std::endl;
+    std::cout << estimator.get_r() << std::endl;
+    std::cout << estimator.get_force_bias().transpose() << std::endl;
+    std::cout << estimator.get_torque_bias().transpose() << std::endl;
+    std::cout << estimator.get_accel_bias().transpose() << std::endl;
+
+    return 0;
 }
