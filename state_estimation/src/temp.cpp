66f167c8fe65138546eb69099300f405926ec29e
Jalma <you@example.com>
2024-11-15 18:57:49 +0100

    Kalman Filter Added

---
+++ state_estimation/src/temp.cpp
@@ -0,0 +1,24 @@
+//
+// Created by jalma on 11/15/24.
+//
+
+#include "temp.hpp"
+
+temp::temp(float m_hat, Eigen::VectorXd &r_hat){
+    m_identity = Eigen::MatrixXd::Identity(9, 9);
+    m_A_k = Eigen::MatrixXd::Identity(9, 9);
+    m_B_k = Eigen::MatrixXd::Zero(9, 3);
+
+    m_B_k << Eigen::MatrixXd::Identity(3, 3),
+            Eigen::MatrixXd::Identity(3, 3)*m_hat,
+            skewSymmetric(r_hat)*m_hat;
+
+}
+
+Eigen::Matrix3d temp::skewSymmetric(const Eigen::Vector3d &v){
+    Eigen::Matrix3d skewMat = Eigen::Matrix3d::Zero();
+    skewMat.col(0) = v.cross(Eigen::Vector3d::UnitX());
+    skewMat.col(1) = v.cross(Eigen::Vector3d::UnitY());
+    skewMat.col(2) = v.cross(Eigen::Vector3d::UnitZ());
+    return skewMat;
+}
